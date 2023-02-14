adc1e4838ad9dd8a11b5bdb8302cba13e51aa56b
WoolfDeveloper <lluclanser@gmail.com>
2023-02-14 20:10:12 +0100

    AAAAAAAAAA

--- movie-app-main/app.js
+++ movie-app-main/app.js
@@ -1,4 +1,4 @@
-const API_KEY = `04ec85a684eae5ff819855d99f118b3b`
+const API_KEY = `b4e59077eed37925947989634404ea03`
 const image_path = `https://image.tmdb.org/t/p/w1280`
 
 // - API key will only be available for this video since I'll delete it right after that but you can go to https://www.themoviedb.org/ , create an account and get a key for free.
